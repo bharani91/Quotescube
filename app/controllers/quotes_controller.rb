12f892ea082411a501f9ed698064353f7a9cb9fa
Bharani M <bharani91@gmail.com>
2012-06-17 21:26:14 +0530

    keyboard shortcuts

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,7 +3,6 @@ class QuotesController < ApplicationController
   # GET /quotes.json
   def index
     @quote = Quote.first(:offset => rand(Quote.count))
-    @authors = Quote.uniq.pluck(:author)
     respond_to do |format|
       format.html # index.html.erb
       format.js
@@ -31,7 +30,6 @@ class QuotesController < ApplicationController
   # GET /author/:name.json
   def quotes_by_author
     @quotes = Quote.where(:author_slug => params[:name]).paginate(:page => params[:page], :per_page => 15)
-    @authors = Quote.uniq.pluck(:author)
     @author = @quotes.first.author unless @quotes.empty?
     respond_to do |format|
       format.html # show.html.erb
@@ -43,7 +41,6 @@ class QuotesController < ApplicationController
   # GET /category/:cat.json
   def quotes_by_category
     @quotes = Quote.where(:category => params[:cat]).paginate(:page => params[:page], :per_page => 21)
-    @authors = Quote.uniq.pluck(:author)
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @quote }
